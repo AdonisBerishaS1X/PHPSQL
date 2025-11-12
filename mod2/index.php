dbf1e86684696a8ac20ff656ca7b052d5f53d485
AdonisBerishaS1X <rubingducks@gmail.com>
2025-11-12 18:53:06 +0100

    mod2

---
+++ mod2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>My first PHP page</h1>
+
+    <?php 
+    //  $school="Imperium of Man";
+    //  $var1;
+    //  var_dump($var1);
+    //  echo $school;
+
+    // $x=120;
+    // $y=50;
+    // echo $x+$y."<br>";
+    // echo $x-$y."<br>";
+    // echo $x*$y."<br>";
+    // echo $x/$y."<br>";
+
+    // $a="Digital";
+    // $b="School";  
+
+    $programming="Programing is not cool";
+
+    echo str_replace("not", "ver", $programming)."<br>";
+
+    exho str_wrd_count($programming).'<br>';
+
+    echo strlen($programming);
+    ?>
+    
+</body>
+</html>

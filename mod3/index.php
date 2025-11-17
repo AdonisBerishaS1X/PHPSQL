adff9809c1fe7b64861da9db0d864de8f4cce874
AdonisBerishaS1X <rubingducks@gmail.com>
2025-11-17 19:04:56 +0100

    moduli3

---
+++ mod3/index.php
@@ -0,0 +1,70 @@
+<?php 
+ 
+//  $num=1;
+
+//  if($num<0){
+//     echo "$num is less than 0";
+//  }
+
+//  $age = 21;
+//  if ($age<29){
+//     echo "You are under 2;"
+//  }else{
+//     echo "You are an adult";
+//  }
+
+// $age = 15;
+// if(($age<12) && ($age<20)){
+//     echo "You are a teenager";
+// }
+
+// if (($age<12) || ($age<20)){
+//     echo "You are and adult";
+// }
+
+// $age="hello";
+// switch($age){
+//     case($age>=0 && $age<18):
+//         echo "You are a minor";
+//         break;
+//     case($age>=18 && $age<25):
+//         echo "You are a young adult";
+//         break;
+//     case($age>=25):
+//         echo "You are a adult";
+//         break;
+
+//     default:
+//     echo "Invalid Age";
+//     break;
+// }
+
+// $x=1;
+
+// while($x<=5){
+//     echo "The numer is:$ <br>";
+
+//     $x=$x+1;
+// }
+
+// do{
+//     echo "The numer is:$x <br>";
+
+//     $s++;
+// }while($x<=5);
+
+for($x=0;$x=10;$x++){
+    // echo "The number is:$x <br>";
+}
+
+$cars=['BMS','AUDI','VW','TESLA'];
+foreach($cars as $value){
+    // echo "$value <br>";
+}
+
+$age=["John"=>'18', "Geat"=>'12','Rini'=>'16'];
+
+forach($age as $s=>$val){
+    echo "$x=$val <br";
+}
+?>

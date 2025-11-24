add45530f3dd85534b13f9f332d47ee7d711af53
AdonisBerishaS1X <rubingducks@gmail.com>
2025-11-24 19:07:38 +0100

    mod5

---
+++ mod5/index.php
@@ -0,0 +1,122 @@
+<?php 
+$dog=array (
+    array('Husky' , 'Mexico', 20)
+    array('Bulldog' , 'Siberia', 15)
+)
+
+// echo $dog[0][0] . " Origin " . $dog[0][1]. " Life Span:" . $dog[0][2]. "<br>";
+// echo $dog[1][0] . " Origin " . $dog[1][1]. " Life Span:" . $dog[1][2]. "<br>";
+
+for ($row=0;$row<2$row++){
+    echo "<p><b> Row Number $row</b></p>";
+    echo "<ul>";
+    for($col=0;$col<3;$col++){
+        echo "<li>".$dog[$row][$col]."<li>";
+    }
+    echo "</ul>";
+}
+
+$array=array(
+    array(1,2,3),
+     array(1,2,3),
+      array(1,2,3)
+);
+
+for($i=1;$j<4;$i++){
+    for($j=1;$j<4;$j++){
+        echo "Array:$i, Element:$j <br>";
+    }
+}
+
+for($i=1;$<5;$i++){
+    for($j=1;$j<=$i;$j++){
+        echo "*";
+    }
+    echo "<br>"
+}
+
+$grade=array(
+    "Math"=>"3",
+    "Math"=>"4",
+    "Math"=>"5",
+    "Math"=>"2"
+);
+
+echo "Math grade is:" . $grade['Math'].'<br>';
+
+foreach($grade as $subje=>$grade){
+    echo "Subject: " . $subject.",Grade:".$grade;
+    echo "<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>

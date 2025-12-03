94051bff262c4317376958fd4a5ac56549cc3cf5
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-12-03 19:06:30 +0100

    Mod8

---
+++ mod8/add.php
@@ -0,0 +1,18 @@
+<?php 
+$host="localhost";
+$db="lure";
+$user="root";
+$pass="";
+
+try{
+    $pdo=new PDO("mysql:host=$host; dbname=$db", $user,$pass);
+
+    // $sql="ALTER TABLE users ADD email varchar(255)";
+    $sql="ALTER TABLE users DROP COLUMN username";
+
+    $pdo->exec($sql);
+    echo "Column Created Successfully";
+}catch(PDOException $e){
+    echo "Error:". $e->getMessage();
+}
+?>

02cd4d425114f82792d802bad64f66c749586250
AdonisBerishaS1X <rubingducks@gmail.com>
2025-12-17 19:01:01 +0100

    Mod12

--- mod12/dashboard.php
+++ mod12/dashboard.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <style>
         table,th,td{
             border:1px solid black;
@@ -15,6 +16,8 @@
     </style>
 </head>
 <body>
+
+<?php include_once{"Header.php"};?>
     <?php 
     include_once("config.php");
     $sql="SELECT * FROM users";
@@ -46,5 +49,8 @@
     </tbody>
 </table>
 <a href="add.php">Add</a>
+
+<?php include_once{"footer.php"};?>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </html>
